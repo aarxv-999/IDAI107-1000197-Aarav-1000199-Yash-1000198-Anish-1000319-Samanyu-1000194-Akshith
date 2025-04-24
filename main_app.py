c2b357386c73e1c326dc4b2f42d5699e9ed0947a
aarxv-999 <aarxv999@gmail.com>
2025-04-24 10:12:28 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,1 +1,13 @@
+#aarav
+import os
+from dotenv import load_dotenv
+import streamlit as st
 
+load_dotenv()
+st.set_page_config(page_title="Smart Restaurant")
+
+feature = st.sidebar.selectbox("Feature", [
+    "Recipe", "Leftover", "Promotions", "Event", "Visual Search"
+])
+
+st.write("Select a feature to get started.")
