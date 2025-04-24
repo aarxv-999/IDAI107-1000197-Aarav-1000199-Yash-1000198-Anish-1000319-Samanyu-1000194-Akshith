0c12ab3134e58ae7783d55f028508777802d5b17
aarxv-999 <aarxv999@gmail.com>
2025-04-24 10:11:54 +0530

    Update components.py

--- ui/components.py
+++ ui/components.py
@@ -1,1 +1,10 @@
+# aarav
+import streamlit as st
 
+def show_leaderboard(data: dict):
+    """TODO: display a leaderboard widget."""
+    st.write("Leaderboard placeholder", data)
+
+def upload_image_widget():
+    """TODO: image uploader widget."""
+    return st.file_uploader("Upload a dish image")
