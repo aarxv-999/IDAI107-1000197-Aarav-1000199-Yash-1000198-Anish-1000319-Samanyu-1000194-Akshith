de2fd890b523fd0341d8bcad416c5857d03297fd
aarxv-999 <aarxv999@gmail.com>
2025-07-17 19:06:34 +0530

    Update firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -1,13 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials
 import streamlit as st
-import json
 
 def init_firebase():
     if not firebase_admin._apps:
         try:
             with open("firebase_cred.json") as f:
-                config_dict = json.load(f)
+                config_dict = dict(st.secrets["firebase"])
             cred = credentials.Certificate(config_dict)
             firebase_admin.initialize_app(cred)
             return True
